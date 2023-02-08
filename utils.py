4739d507d1c4174c631ed88d32151c87ecdb93c3
Nick_Linux <n.avramenko87@gmail.com>
2023-02-08 23:26:37 +0300

    add functions of checking

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+test_dict = {'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041', 'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'}
 
 def load_data(file_name: str) -> list:
     """
@@ -9,9 +10,26 @@ def load_data(file_name: str) -> list:
     :return: list of dictionaries
     """
     path_file = os.path.join(os.getcwd(), "data", file_name)
+
     file = open(path_file, 'r', encoding='utf-8')
     data = json.load(file)
     file.close()
     return data
 
-print(load_data("operations.json"))
+def check_executed_transaction(transaction: dict) -> bool:
+    if transaction.get("state") == "EXECUTED":
+        return True
+    return False
+
+data = load_data("operations.json")
+
+
+def get_all_executed_transactions(transactions: list) -> list:
+    all_executed_transactions = []
+    for transaction in transactions:
+        if check_executed_transaction(transaction) is True:
+            all_executed_transactions.append(transaction)
+    return all_executed_transactions
+
+data_executed = get_all_executed_transactions(data)
+
